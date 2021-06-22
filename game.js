d28fc55be7f23aca5395bd45e80b2ec8a4d94eae
Nishant Mourya <2019kuec2010@iiitkota.ac.in>
2021-06-23 03:18:20 +0530

    added new features

--- game.js
+++ game.js
@@ -5,16 +5,15 @@ element.forEach(function (el) {
         if (i === 0 && el.textContent == "_") {
             el.innerHTML = "X";
             i++;
-            j = document.getElementById("c1a").textContent;
-
-            k = document.getElementById("c1b").textContent;
-            l = document.getElementById("c1c").textContent;
-            m = document.getElementById("c2a").textContent;
-            n = document.getElementById("c2b").textContent;
-            o = document.getElementById("c2c").textContent;
-            p = document.getElementById("c3a").textContent;
-            q = document.getElementById("c3b").textContent;
-            r = document.getElementById("c3c").textContent;
+            j = document.getElementById("c11").textContent;
+            k = document.getElementById("c12").textContent;
+            l = document.getElementById("c13").textContent;
+            m = document.getElementById("c14").textContent;
+            n = document.getElementById("c15").textContent;
+            o = document.getElementById("c16").textContent;
+            p = document.getElementById("c17").textContent;
+            q = document.getElementById("c18").textContent;
+            r = document.getElementById("c19").textContent;
             checkwin();
            
         }
@@ -23,15 +22,15 @@ element.forEach(function (el) {
             if (el.textContent == "_") {
                 el.innerHTML = "O";
                 i = 0;
-                j = document.getElementById("c1a").textContent;
-                k = document.getElementById("c1b").textContent;
-                l = document.getElementById("c1c").textContent;
-                m = document.getElementById("c2a").textContent;
-                n = document.getElementById("c2b").textContent;
-                o = document.getElementById("c2c").textContent;
-                p = document.getElementById("c3a").textContent;
-                q = document.getElementById("c3b").textContent;
-                r = document.getElementById("c3c").textContent;
+                j = document.getElementById("c11").textContent;
+                k = document.getElementById("c12").textContent;
+                l = document.getElementById("c13").textContent;
+                m = document.getElementById("c14").textContent;
+                n = document.getElementById("c15").textContent;
+                o = document.getElementById("c16").textContent;
+                p = document.getElementById("c17").textContent;
+                q = document.getElementById("c18").textContent;
+                r = document.getElementById("c19").textContent;
                 checkwin();
                 
             }
@@ -45,89 +44,118 @@ var over;
 function checkwin() {
     if (j == "X" && k == "X" && l == "X") {
         document.querySelector(".head22").innerHTML = "X WON!!! by Column1";
+        setdisable();
+        
         gameover();
+        
         return 0;
     }
     if (j == "O" && k == "O" && l == "O") {
         document.querySelector(".head22").innerHTML = "O WON!!! by Column1";
+        setdisable();
         gameover();
         return 0;
     }
     if (m == "O" && n == "O" && o == "O") {
         document.querySelector(".head22").innerHTML = "O WON!!! by Column2";
+        setdisable();
         gameover();
         return 0;
     }
     if (m == "X" && n == "X" && o == "X") {
         document.querySelector(".head22").innerHTML = "X WON!!! by Column2";
+        setdisable();
         gameover();
         return 0;
     }
     if (p == "X" && q == "X" && r == "X") {
         document.querySelector(".head22").innerHTML = "X WON!!! by Column3";
+        setdisable();
         gameover();
         return 0;
     }
     if (p == "O" && q == "O" && r == "O") {
         document.querySelector(".head22").innerHTML = "O WON!!! by Column3";
+        setdisable();
         gameover();
         return 0;
     }
 
     if (j == "X" && m == "X" && p == "X") {
         document.querySelector(".head22").innerHTML = "X WON!!! by Row1";
+        setdisable();
         gameover();
         return 0;
     }
     if (j == "O" && m == "O" && p == "O") {
         document.querySelector(".head22").innerHTML = "O WON!!! by Row1";
+        setdisable();
         gameover();
         return 0;
     }
     if (k == "O" && n == "O" && q == "O") {
         document.querySelector(".head22").innerHTML = "O WON!!! by Row2";
+        setdisable();
         gameover();
         return 0;
     }
     if (k == "X" && n == "X" && q == "X") {
         document.querySelector(".head22").innerHTML = "X WON!!! by Row2";
+        setdisable();
         gameover();
         return 0;
     }
     if (l == "O" && o == "O" && r == "O") {
         document.querySelector(".head22").innerHTML = "O WON!!! by Row3";
+        setdisable();
         gameover();
         return 0;
     }
     if (l == "X" && o == "X" && r == "X") {
         document.querySelector(".head22").innerHTML = "X WON!!! by Row3";
+        setdisable();
         gameover();
         return 0;
     }
     if (j == "O" && n == "O" && r == "O") {
         document.querySelector(".head22").innerHTML = "O WON!!! by Left Diagonal";
+        setdisable();
         gameover();
         return 0;
     }
 
     if (j == "X" && n == "X" && r == "X") {
         document.querySelector(".head22").innerHTML = "X WON!!! by Left Diagonal";
+        setdisable();
         gameover();
         return 0;
     }
     
     if (p == "O" && n == "O" && l == "O") {
         document.querySelector(".head22").innerHTML = "O WON!!! by Right Diagonal";
+        setdisable();
         gameover();
         return 0;
     }
     if (p == "X" && n == "X" && l == "X") {
         document.querySelector(".head22").innerHTML = "X WON!!! by Right Diagonal";
+        setdisable();
         gameover();
         return 0;
     }
  
 }
+
+function setdisable(){
+
+        for(var z=11;z<20;z++){
+            document.getElementById("c" + z).setAttribute("disabled", "true");
+        }
+    
+    
+}
 function gameover(){
     document.getElementById("gameover").innerHTML="<p>GAME OVER click RESET to play a New GAME!</p>";
+    var audio = new Audio("sounds/gameover.mpeg");
+    audio.play();
 }
